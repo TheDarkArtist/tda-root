2bfaa4efa36bd5c5f6bf80ace467b56060ba52e5
Kushagra <sparrow.kushagra@gmail.com>
2024-08-07 13:41:12 +0530

    Add reusable search component
    It can be used with any kind of list it takes an input and sets it as
    query in the search bar that can be used to search for query by using
    nextjs's useSearchParams

---
+++ src/components/utils/search.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
+interface SearchProps {
+  placeholder?: string;
+  className?: string;
+}
+
+const Search: React.FC<SearchProps> = ({ placeholder, className }) => {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
+  return (
+    <input
+      className={cn(
+        "py-2 px-4 rounded-sm dark:bg-zinc-950 bg-white shadow border dark:border-stone-600/60 border-stone-300 w-full mx-auto",
+        className
+      )}
+      placeholder={placeholder || "Search"}
+      defaultValue={searchParams.get("query")?.toString() as string}
+      onChange={(e) => {
+        handleSearch(e.target.value);
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    />
+  );
+};
+
+export default Search;
