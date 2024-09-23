17c44ec0c49c762325ac98c7e9196f3dbab0b4e3
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-09-23 23:53:04 +0530

    Modify font from poppins to Open Sans

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -1,11 +1,11 @@
 import { cn } from "@/lib/utils";
-import { poppins } from "@/utils/fonts";
+import { os } from "@/utils/fonts";
 import React from "react";
 
 const Header = ({ label }: { label: string }) => {
   return (
     <div className="flex flex-col items-center gap-y-4 w-full text-zinc-800 dark:text-zinc-300">
-      <h1 className={cn("text-4xl font-semibold", poppins.className)}>
+      <h1 className={cn("text-4xl font-semibold", os.className)}>
         Authentication
       </h1>
       <p className="text-sm">{label}</p>
