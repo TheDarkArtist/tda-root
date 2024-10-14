da9414ec62ef9b6fa2b2ea88c1e32f33798600a4
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-15 05:25:45 +0530

    Enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/navbar/navbar";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import { os } from "@/utils/fonts";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Kushagra Sharma",
@@ -33,6 +34,7 @@ export default function RootLayout({
             {children}
             <Toaster />
           </Providers>
+          <Analytics />
         </body>
       </SessionProvider>
     </html>
