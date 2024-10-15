75352e485d31b25f54dcd4b2bee28fb123c34290
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-16 00:51:21 +0530

    api/project,post Improve APIs with fetch by dynaimc id and params

--- src/app/api/data/projects/route.ts
+++ src/app/api/data/projects/route.ts
@@ -1,7 +1,24 @@
 import { getProjects } from "@/lib/actions/projects/get-projects";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
-  const projects = await getProjects();
-  return NextResponse.json(projects);
+export const GET = async (req: NextRequest) => {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const publishedParam = searchParams.get("published");
+    const isPublished =
+      publishedParam === null ? undefined : publishedParam === "true";
+
+    const limit = parseInt(searchParams.get("limit") || "10", 10);
+
+    const projects = await getProjects(isPublished, undefined, limit);
+
+    return NextResponse.json(projects, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
 };
