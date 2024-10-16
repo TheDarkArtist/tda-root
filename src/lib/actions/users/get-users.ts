476d7ff9cf6198505b4645b7fe8ecc7d8105f05c
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-17 04:13:11 +0530

    actions seperate user server action logic

---
+++ src/lib/actions/users/get-users.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { db } from "@/lib/db";
+
+export const getUsers = async () => {
+  try {
+    const users = await db.user.findMany();
+    return users;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return null;
+  }
+};
