cd99ba915b825146a9d4ced9f890b48e5b00295d
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-17 04:07:43 +0530

    types Add new custom ProjectWithUserView type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,3 +3,7 @@ import { Prisma } from "@prisma/client";
 export type PostWithUserViews = Prisma.PostGetPayload<{
   include: { user: true; views: true };
 }>;
+
+export type ProjectWithUserViews = Prisma.ProjectGetPayload<{
+  include: { comments: true; views: true };
+}>;
