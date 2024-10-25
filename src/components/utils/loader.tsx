0f9e0639b666dbdb410fda437f2580858441aeaa
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-25 19:11:19 +0530

    Refactor components/utils/loader now acceps more props

--- src/components/utils/loader.tsx
+++ src/components/utils/loader.tsx
@@ -3,8 +3,24 @@
 import React from "react";
 import { RotatingLines } from "react-loader-spinner";
 
-const Loader = ({ color = "gray" }: { color?: string }) => {
-  return <RotatingLines width="24" strokeColor={color} strokeWidth="4" />;
+interface LoaderProps {
+  color?: string;
+  width?: string;
+  strokeWidth?: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({
+  color = "gray",
+  width = "24",
+  strokeWidth = "4",
+}) => {
+  return (
+    <RotatingLines
+      width={width}
+      strokeColor={color}
+      strokeWidth={strokeWidth}
+    />
+  );
 };
 
 export default Loader;
