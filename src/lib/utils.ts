3d35d1de994472744ada06e8bf284459fd9bf0ad
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-25 21:49:28 +0530

    Add slugify function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,14 @@ export function cn(...inputs: ClassValue[]) {
 export const generateCuid = (): string => {
   return cuid();
 };
+
+export function slugify(text: string) {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/[\s_]+/g, "-") // Replace spaces and underscores with hyphens
+    .replace(/[^\w\-]+/g, "") // Remove non-alphanumeric characters except hyphens
+    .replace(/\-\-+/g, "-") // Replace multiple hyphens with a single hyphen
+    .replace(/^-+|-+$/g, ""); // Trim hyphens from start and end
+}
