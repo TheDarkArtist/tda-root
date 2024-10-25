0230570fb855c3ab424b3b62c87f7f21a42fde59
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-25 21:51:37 +0530

    Fix editor/header removed shadcn-ui bredcrum implemented my own There were some issue, the page was getting reloaded due to some improper props passing, i was to lazy to figure out the underlying issue so i just replaced it with some divs and links

--- src/components/editor/header/header.tsx
+++ src/components/editor/header/header.tsx
@@ -1,37 +1,21 @@
 import React from "react";
 import SaveButton from "./save-button";
 import PublishButton from "./publish-button";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { LuChevronRight } from "react-icons/lu";
 
 const Header = ({ type, slug }: { type: "project" | "post"; slug: string }) => {
   return (
-    <div className=" sticky top-0 gap-2 border-b dark:border-zinc-800 border-zinc-200 flex flex-wrap sm:flex-nowrap justify-between items-center w-full dark:bg-zinc-900 bg-zinc-100 p-2 min-h-10">
-      <div>
-        <Breadcrumb>
-          <BreadcrumbList>
-            <div className="text-xl">🛠️</div>
-            <BreadcrumbItem>
-              <BreadcrumbLink
-                href={`/${type}s`}
-              >{`All ${type}s`}</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink href={`/${type}s/${slug}`}>{slug}</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Editor</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+    <div className=" sticky top-0 gap-2 border-b dark:border-zinc-800 border-zinc-200 flex flex-wrap sm:flex-nowrap justify-between items-center w-full dark:bg-zinc-900 bg-zinc-100 p-2 min-h-10 sm:max-h-10">
+      <div className="flex items-center sm:text-sm gap-1 text-xs flex-wrap">
+        <div className="text-sm pr-2">🛠️</div>
+        <Link className="opacity-80 hover:opacity-100" href={`/${type}s`}>{`All ${type}s`}</Link>
+        <LuChevronRight />
+        <Link className="opacity-80 hover:opacity-100" href={`/${type}s/${slug}`}>
+          {slug}
+        </Link>
+        <LuChevronRight />
+        <div>Editor</div>
       </div>
       <div className="flex gap-2 justify-end w-full sm:w-min">
         <PublishButton />
