8f88406443ede9e77f8114293dc1c0ab3c4c013c
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-10-30 17:23:08 +0530

    UIRefactor

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -17,30 +17,32 @@ const PostPage = ({
   const currentPage = Number(searchParams?.page) || 1;
   return (
     <main className="h-full flex flex-col justify-between">
-      <div className="px-4 lg:px-0 mt-12 pb-4">
+      <div className="px-4 xl:px-0 mt-12 pb-4">
         <div className="max-w-screen-lg mx-auto w-full">
           <section className="space-y-2 sm:space-y-6">
             <h1 className="text-2xl sm:text-4xl text-green-600 font-black mt-6 hover:animate-pulse">
               Read Blogs & Informative Articles
             </h1>
             <p className="text-sm">
-              This page is all about sharing my perspectives, insights, and
-              experiences on a wide range of topics, with a focus on technology
-              and beyond. Through my blog posts, I aim to offer
-              thought-provoking discussions, personal reflections, and
-              explorations of emerging trends in areas such as artificial
-              intelligence, software development, digital innovation and so much
+              This page is all about sharing my perspectives,
+              insights, and experiences on a wide range of topics,
+              with a focus on technology and beyond. Through my blog
+              posts, I aim to offer thought-provoking discussions,
+              personal reflections, and explorations of emerging
+              trends in areas such as artificial intelligence,
+              software development, digital innovation and so much
               more.
             </p>
             <p>
-              Here, I plan to discuss discuss different books, tutorials and
-              other stuff that might be interesting and fun to discuss, For my
-              projects you should check out the projects page, which includes
-              detailed information about the i&apos;ve done.
+              Here, I plan to discuss discuss different books,
+              tutorials and other stuff that might be interesting and
+              fun to discuss, For my projects you should check out the
+              projects page, which includes detailed information about
+              the i&apos;ve done.
             </p>
           </section>
 
-          <section className="sticky bg-white dark:bg-zinc-950 h-min z-20 my-10 py-4 top-12 flex gap-4 items-center">
+          <section className="bg-white dark:bg-zinc-950 h-min z-20 my-10 p-4 flex gap-4 items-center rounded-md">
             <Search placeholder="Search posts..." />
 
             <Suspense fallback={<div></div>}>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -17,20 +17,23 @@ const ProjectsPage = ({
   const currentPage = Number(searchParams?.page) || 1;
   return (
     <main className="h-full flex flex-col justify-between">
-      <div className="px-4 lg:px-0 mt-16 pb-4">
+      <div className="px-4 xl:px-0 mt-16 pb-4">
         <div className="max-w-screen-lg mx-auto w-full">
           <section className="space-y-6">
-            <h1 className="text-4xl text-sky-600 font-black">Projects</h1>
+            <h1 className="text-4xl text-sky-600 font-black">
+              Projects
+            </h1>
             <p>
-              This is my personal space where I showcase my self-developed
-              projects and blog about my private coding adventures. It’s updated
-              when I feel like posting bigger news to the world, for more
-              frequent and smaller updates, follow me on Instagram or X. All of
-              my public source are available over at my github repository.
+              This is my personal space where I showcase my
+              self-developed projects and blog about my private coding
+              adventures. It’s updated when I feel like posting bigger
+              news to the world, for more frequent and smaller
+              updates, follow me on Instagram or X. All of my public
+              source are available over at my github repository.
             </p>
           </section>
 
-          <section className="sticky bg-white dark:bg-zinc-950 h-min z-20 my-10 py-4 top-12 flex gap-4 items-center">
+          <section className="bg-white dark:bg-zinc-950 h-min z-20 my-10 p-4 flex gap-4 items-center rounded-md">
             <Search placeholder="Search Projects..." />
 
             <Suspense fallback={<div></div>}>
