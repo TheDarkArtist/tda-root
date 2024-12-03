c03c285fb52e70044fdacadeb616c074496f68f1
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-12-04 01:49:11 +0530

    post/card/body Fix UI

--- src/components/posts/card/body.tsx
+++ src/components/posts/card/body.tsx
@@ -8,7 +8,7 @@ const Body = ({ post }: { post: Post }) => {
     <>
       <CardHeader className="text-sky-600 text-sm sm:text-base md:text-xl font-black p-0">
         <span className="hidden sm:block break-all leading-none">
-          {post.title?.substring(0, 62)}
+          {post.title?.substring(0, 60)}
           {post.title?.substring(60) && <span>...</span>}
         </span>
         <span className="sm:hidden text-base break-all leading-none">
@@ -17,13 +17,17 @@ const Body = ({ post }: { post: Post }) => {
         </span>
       </CardHeader>
       <CardDescription>
-        <span className="hidden sm:text-xs sm:block break-all">
-          {Md2Text(post.description?.substring(0, 115) as string)}
-          {post.description?.substring(300) && <span>...</span>}
-        </span>
         <span className="sm:hidden text-xs break-all">
           {post.description?.substring(0, 150)}
           {post.description?.substring(150) && <span>...</span>}
+        </span>
+        <span className="hidden sm:text-xs lg:hidden sm:block break-all">
+          {Md2Text(post.description?.substring(0, 115) as string)}
+          {post.description?.substring(300) && <span>...</span>}
+        </span>
+        <span className="hidden text-xs lg:block break-all">
+          {Md2Text(post.description?.substring(0, 480) as string)}
+          {post.description?.substring(480) && <span>...</span>}
         </span>
       </CardDescription>
     </>
