d370080293e11394d1592ed46f357fda2d5cd4f6
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-12-10 02:36:18 +0530

    refactor(utils/buttons/publish-button): Optimized it.

--- src/components/utils/buttons/publish-button.tsx
+++ src/components/utils/buttons/publish-button.tsx
@@ -3,25 +3,24 @@
 import { Button } from "@/components/ui/button";
 import { updatePost } from "@/lib/actions/posts/update-post";
 import { updateProject } from "@/lib/actions/projects/update-project";
-import { PostWithUserViews, ProjectWithUserViews } from "@/lib/types";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import { toast } from "sonner";
 
 interface PublishButtonProps<T extends "project" | "post"> {
   type: T;
-  post: T extends "project" ? ProjectWithUserViews : PostWithUserViews;
+  id: string;
+  published: boolean;
   className?: string;
 }
 
 const PublishButton: React.FC<PublishButtonProps<"project" | "post">> = ({
   type,
-  post,
+  published,
+  id,
   className,
 }) => {
   const [isPending, setIsPending] = useState(false);
-  const { views, user, ...postWithoutUser } = post;
-  const [localPost, setLocalPost] = useState(postWithoutUser);
 
   const { status } = useSession();
 
@@ -32,7 +31,7 @@ const PublishButton: React.FC<PublishButtonProps<"project" | "post">> = ({
   const handlePublishToggle = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    if (!localPost) {
+    if (!id) {
       toast.error("No data available.");
       return;
     }
@@ -40,18 +39,14 @@ const PublishButton: React.FC<PublishButtonProps<"project" | "post">> = ({
     setIsPending(true);
 
     try {
-      const updatedData = { ...localPost, published: !localPost.published };
-
-      setLocalPost(updatedData);
-
       if (type === "project") {
-        await updateProject(updatedData.id, updatedData);
+        await updateProject(id, { published: !published });
       } else if (type === "post") {
-        await updatePost(updatedData.id, updatedData);
+        await updatePost(id, { published: !published });
       }
 
       const message = `${type.charAt(0).toUpperCase() + type.slice(1)} ${
-        updatedData.published ? "Published!" : "Unpublished!"
+        published ? "Published!" : "Unpublished!"
       }`;
       toast.success(message);
     } catch (error) {
@@ -70,11 +65,7 @@ const PublishButton: React.FC<PublishButtonProps<"project" | "post">> = ({
         size="sm"
         disabled={isPending}
       >
-        {isPending
-          ? "Updating..."
-          : localPost.published
-            ? "Unpublish"
-            : "Publish"}
+        {isPending ? "Updating..." : published ? "Unpublish" : "Publish"}
       </Button>
     </form>
   );
