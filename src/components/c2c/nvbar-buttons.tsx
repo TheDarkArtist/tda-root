5bf2f08ec134cef0ad622c5bbc13849ec7ddbdb0
Kushagra Sharma <sparrow.kushagra@gmail.com>
2024-12-10 02:45:31 +0530

    feat(c2c/navbar/nav-buttons): Add create user, post buttons

--- src/components/c2c/nvbar-buttons.tsx
+++ src/components/c2c/nvbar-buttons.tsx
@@ -3,23 +3,36 @@
 import { usePathname } from "next/navigation";
 import React from "react";
 import { Button } from "../ui/button";
+import Link from "next/link";
+import { generateCuid } from "@/lib/utils";
 
 const NvbarButtons = () => {
   const pathname = usePathname();
 
+  const slug = generateCuid();
+
   if (pathname.includes("users")) {
+    // TODO: implement this functionality kinda like a modal
     return <Button size="sm">Add User</Button>;
   }
 
-  if (pathname.includes("posts")) {
-    return <Button size="sm">Add Post</Button>;
+  if (pathname.includes("post")) {
+    return (
+      <Button asChild size="sm">
+        <Link href={`/posts/${slug}/edit`}>Add Post</Link>
+      </Button>
+    );
   }
 
   if (pathname.includes("projects")) {
-    return <Button size="sm">Add Project</Button>;
+    return (
+      <Button asChild size="sm">
+        <Link href={`/projects/${slug}/edit`}>Add Project</Link>
+      </Button>
+    );
   }
 
-  return null;
+  return <div>NvbarButtons</div>;
 };
 
 export default NvbarButtons;
